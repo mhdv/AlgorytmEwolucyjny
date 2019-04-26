4e8576110677336789a2774d88f5d4d85576874a
mhdv <kontakt@mhdv.pl>
2019-04-26 18:00:18 +0200

    init

---
+++ AlgorytmEwolucyjny/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AlgorytmEwolucyjny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string equationString;
+        public Expression equation;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEquation_Click(object sender, RoutedEventArgs e)
+        {
+            equationString = txtEquation.Text;
+            equation = new Expression(equationString);
+        }
+    }
+}
